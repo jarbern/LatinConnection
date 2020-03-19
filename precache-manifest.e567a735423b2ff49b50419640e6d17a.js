defb5835830fc8ce6334ba2e56711f55f02fe039
jarbern <jared.bernhart@gmail.com>
2020-03-19 10:41:24 -0400

    Updates

---
+++ precache-manifest.e567a735423b2ff49b50419640e6d17a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7551842dc4b28f070f5127174b3705f8",
+    "url": "/LatinConnection/index.html"
+  },
+  {
+    "revision": "0019d205fe16ff9806ee",
+    "url": "/LatinConnection/static/css/main.fef28abd.chunk.css"
+  },
+  {
+    "revision": "5a215844bb366ae42b29",
+    "url": "/LatinConnection/static/js/2.6b6dc447.chunk.js"
+  },
+  {
+    "revision": "dd30ad2c00174e59bb32191ea757f584",
+    "url": "/LatinConnection/static/js/2.6b6dc447.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0019d205fe16ff9806ee",
+    "url": "/LatinConnection/static/js/main.53bb0337.chunk.js"
+  },
+  {
+    "revision": "87efc7bdfa30485bed3a",
+    "url": "/LatinConnection/static/js/runtime-main.d4eb1ace.js"
+  },
+  {
+    "revision": "8d5a7f0d81f54124f2629976c4c23683",
+    "url": "/LatinConnection/static/media/Hero.8d5a7f0d.jpg"
+  },
+  {
+    "revision": "0859a70806496fe3709c7083391bff75",
+    "url": "/LatinConnection/static/media/Lofts.0859a708.jpg"
+  },
+  {
+    "revision": "f9ed36b4484cd731cad87d38899a2a72",
+    "url": "/LatinConnection/static/media/brownstone.f9ed36b4.jpg"
+  },
+  {
+    "revision": "64648572090328df58e0f5b904101a46",
+    "url": "/LatinConnection/static/media/modern-lobby.64648572.jpg"
+  },
+  {
+    "revision": "4447401c5f8852a86a36dddb6bfff8de",
+    "url": "/LatinConnection/static/media/pool.4447401c.jpg"
+  },
+  {
+    "revision": "4a01219d42f358a6655da071349a6782",
+    "url": "/LatinConnection/static/media/reading.4a01219d.jpg"
+  }
+]);
